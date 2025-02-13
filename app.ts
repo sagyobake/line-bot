7cf9cd7cc4dc0f619c5a3f2ff5b2965fad609cdf
sagyobake <sagyobake@localhost.localdomain>
2025-02-14 00:24:26 +0900

    upgrade

--- app.ts
+++ app.ts
@@ -21,71 +21,25 @@ await fetch('https://weather.tsukumijima.net/api/forecast/city/130010')
     });
 
 
+    
 
-import { serve } from "https://deno.land/std@0.117.0/http/server.ts";
+import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
+const { LINE_ACCESS_TOKEN } = config();
 
-const accessToken = Deno.env.get("ACCESS_TOKEN");
-const channelSecret = Deno.env.get("CHANNEL_SECRET");
+const url = "https://notify-api.line.me/api/notify";
 
-const enc = new TextEncoder();
-const algorithm = { name: "HMAC", hash: "SHA-256" };
+const body: URLSearchParams = new URLSearchParams({
+    message: "hello from deno!",
+});
 
-async function hmac(secret: string, body: string) {
-    const key = await crypto.subtle.importKey(
-        "raw",
-        enc.encode(secret),
-        algorithm,
-        false,
-        ["sign", "verify"],
-    );
-    const signature = await crypto.subtle.sign(
-        algorithm.name,
-        key,
-        enc.encode(body),
-    );
-    return btoa(String.fromCharCode(...new Uint8Array(signature)));
-}
+const res = await fetch(url, {
+    method: "POST",
+    headers: {
+        "Authorization": `Bearer ${LINE_ACCESS_TOKEN}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body,
+});
 
-async function webhook(request: Request) {
-    if (!accessToken) {
-        throw new Error("ACCESS_TOKEN is not set");
-    }
-    if (!channelSecret) {
-        throw new Error("CHANNEL_SECRET is not set");
-    }
-
-    const json = await request.text();
-    const digest = await hmac(channelSecret, json);
-    const signature = request.headers.get("x-line-signature");
-
-    if (digest !== signature) {
-        return new Response("Bad Request", { status: 400 });
-    }
-
-    const event = JSON.parse(json);
-    console.log(event);
-    if (event.events.length === 0) {
-        return new Response("OK");
-    }
-
-    const res = await fetch("https://api.line.me/v2/bot/message/reply", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-            "authorization": `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-            replyToken: event.events[0].replyToken,
-            messages: [
-                {
-                    type: 'text',
-                    text: yoho,
-                },
-            ],
-        }),
-    });
-    await res.arrayBuffer();
-    return new Response("OK");
-}
-
-
+const json = await res.json();
+console.log(json);
