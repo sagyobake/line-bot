c4d5f19f48ae76fef3d7711da90882c792817e10
sagyobake <sagyobake@localhost.localdomain>
2025-02-13 21:15:31 +0900

    upgrade

--- app.ts
+++ app.ts
@@ -1,100 +1,40 @@
+const url = 'https://discord.com/api/webhooks/1329054269617340437/md41TsLSDH8WTLwFIwYn7p9cDReVw3hQ_E1lz_fpLdy97_C_8xZ3oxlTVIWc0c9endgU';
 
-import { serve } from "https://deno.land/std@0.117.0/http/server.ts";
-
-const accessToken = Deno.env.get("ACCESS_TOKEN");
-const channelSecret = Deno.env.get("CHANNEL_SECRET");
-
-function indexPage() {
-    return new Response(`This is an example LINE bot implementation
-See https://github.com/kt3k/line-bot-deno-deploy for details`);
-}
-
-function notFoundPage() {
-    return new Response("404 Not Found", { status: 404 });
-}
-
-const enc = new TextEncoder();
-const algorithm = { name: "HMAC", hash: "SHA-256" };
-
-async function hmac(secret: string, body: string) {
-    const key = await crypto.subtle.importKey(
-        "raw",
-        enc.encode(secret),
-        algorithm,
-        false,
-        ["sign", "verify"],
-    );
-    const signature = await crypto.subtle.sign(
-        algorithm.name,
-        key,
-        enc.encode(body),
-    );
-    return btoa(String.fromCharCode(...new Uint8Array(signature)));
-}
-
-async function webhook(request: Request) {
-    if (!accessToken) {
-        throw new Error("ACCESS_TOKEN is not set");
-    }
-    if (!channelSecret) {
-        throw new Error("CHANNEL_SECRET is not set");
-    }
-
-    const json = await request.text();
-    const digest = await hmac(channelSecret, json);
-    const signature = request.headers.get("x-line-signature");
-
-    if (digest !== signature) {
-        return new Response("Bad Request", { status: 400 });
-    }
-
-    const event = JSON.parse(json);
-    console.log(event);
-    if (event.events.length === 0) {
-        return new Response("OK");
-    }
-
-    const hoge = (n: number) => {
-        let msg = '';
-        if (n < 18) {
-            msg = '18歳未満の対応は受け付けておりません。'
-        } else {
-            msg = '続いて、性別と現在の職業を記入ください。'
-        }
-        return msg;
-    }
+const tenki_url = "https://map.yahooapis.jp/weather/V1/place?output=json&coordinates=139.50766992096874,35.60448371439913&appid=dj00aiZpPWZNWjMwWWdvenNJWSZzPWNvbnN1bWVyc2VjcmV0Jng9NWQ-"
 
 
-    const res = await fetch("https://api.line.me/v2/bot/message/reply", {
+
+app.post('/webhook', async c => {
+    const data = await c.req.json() // WebHookデータ
+  
+    const replys: Promise<Response>[] = []
+    for (const event of data.events) {
+      // イベントでループ
+      if (event.type !== 'message') return // メッセージでないイベントは無視
+  
+      const { message, replyToken } = event
+  
+      if (message.type !== 'text') return // テキストメッセージでないイベントは無視
+  
+      const textMessage: string = message.text // ユーザーの発言を取得
+  
+      const replyData = {
+        replyToken,
+        messages: [{
+          type: "text",
+          text: `あなたはさっき、${textMessage}と言った！`
+        }],
+      } // リプライするデータを作成
+      replys.push(fetch("https://api.line.me/v2/bot/message/reply", {
         method: "POST",
         headers: {
-            "content-type": "application/json",
-            "authorization": `Bearer ${accessToken}`,
+          "Content-type": "application/json",
+          "Authorization": "Bearer " + Deno.env.get("LINE_TOKEN"),
         },
-        body: JSON.stringify({
-            replyToken: event.events[0].replyToken,
-            messages: [
-                {
-                    type: "text",
-                    text: hoge(event.events[0].message.text),
-                },
-
-            ],
-        }),
-    });
-    await res.arrayBuffer();
-    return new Response("OK");
-}
-
-serve((request) => {
-    const { pathname } = new URL(request.url);
-
-    switch (pathname) {
-        case "/":
-            return indexPage();
-        case "/webhook":
-            return webhook(request);
-        default:
-            return notFoundPage();
+        "body": JSON.stringify(replyData),
+      })) // リプライ
     }
-});
+    await Promise.all(replys) // 全てのリプライ完了を待つ
+  
+    return c.text('OK')
+  })
