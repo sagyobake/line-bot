947ecdb5e4964bb3b7f97f2c398f5f46bdd71a60
sagyobake <sagyobake@localhost.localdomain>
2025-02-14 00:57:06 +0900

    upgrade

--- app.ts
+++ app.ts
@@ -21,27 +21,27 @@ fetch('https://weather.tsukumijima.net/api/forecast/city/130010')
     });
 
 
-
-const postReplyMessage = async (
-    message: string,
-    replyToken: string,
+    
+    const postReplyMessage = async (
+  message: string,
+  replyToken: string,
 ) => {
-    const body = {
-        replyToken,
-        messages: [
-            {
-                type: "text",
-                yoho: message,
-            },
-        ],
-    };
+  const body = {
+    replyToken,
+    messages: [
+      {
+        type: "text",
+        yoho: message,
+      },
+    ],
+  };
 
-    return await fetch("https://api.line.me/v2/bot/message/reply", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `EGLpy+QFWeKGUcWztUfJDDJbW0JKuZco6PxrEKgBKg5wWMR6oRj9luSrUYUw8ho4gySPE7Orox662OipmXwmJP9jNEw58rbJVpuCAgUwDClXXmp+fflNyZTJTgm7yHjy36RCnxNe/B2tgDVhxyM6hgdB04t89/1O/w1cDnyilFU=`,
-        },
-        body: JSON.stringify(body),
-    });
+  return await fetch("https://api.line.me/v2/bot/message/reply", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${CHANNEL_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify(body),
+  });
 };
