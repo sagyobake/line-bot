8ffeb4554d164b593eb311a3960b08ab804f11b7
sagyobake <sagyobake@localhost.localdomain>
2025-02-14 23:45:35 +0900

    upgrade

--- app.ts
+++ app.ts
@@ -61,7 +61,7 @@ app.post("/webhook", async (c) => {
         //ユーザの入力値を取得する
         const input = event.message.text;
 
-        if (input === request) {
+        if (input === result) {
             result = `○`;
         } else {
             result = `✗`;
